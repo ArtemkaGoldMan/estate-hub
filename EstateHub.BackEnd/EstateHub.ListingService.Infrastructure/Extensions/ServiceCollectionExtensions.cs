c49d2b7a324c22717fddfea0969106019beb1051
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2025-12-17 16:32:07 +0100

    refator+

--- EstateHub.BackEnd/EstateHub.ListingService.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ EstateHub.BackEnd/EstateHub.ListingService.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,8 +1,5 @@
 using EstateHub.ListingService.Domain.Interfaces;
-using EstateHub.ListingService.DataAccess.SqlServer.Db;
-using EstateHub.ListingService.DataAccess.SqlServer.Repositories;
 using EstateHub.ListingService.Infrastructure.Services;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Polly;
@@ -12,24 +9,10 @@ namespace EstateHub.ListingService.Infrastructure.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddListingServiceDataAccess(
+    public static IServiceCollection AddListingServiceInfrastructure(
         this IServiceCollection services,
         IConfiguration configuration)
     {
-        // Add DbContext
-        services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-
-        // Add Repositories
-        services.AddScoped<IListingRepository, ListingRepository>();
-        services.AddScoped<ILikedListingRepository, LikedListingRepository>();
-        services.AddScoped<IPhotoRepository, PhotoRepository>();
-        services.AddScoped<IReportRepository, ReportRepository>();
-        services.AddScoped<IAIQuestionUsageRepository, AIQuestionUsageRepository>();
-        
-        // Add UnitOfWork
-        services.AddScoped<EstateHub.SharedKernel.Execution.IUnitOfWork, ListingUnitOfWork>();
-
         // Add Infrastructure Services
         services.AddScoped<ICurrentUserService, CurrentUserService>();
         
